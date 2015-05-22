aa8c499eb960bc176263bdb73e9d7fa200542982
Lorenzo Tan <lorenzo.tan@broadbean.com>
2015-05-22 13:23:12 -0700

    adding database code

--- FamilyTree.java
+++ FamilyTree.java
@@ -1,13 +1,31 @@
 import person.Person;
+import dbi.DBI;
 //import relationship.Relationship;
 import java.util.HashMap;
 
 public class FamilyTree {
+    String FirstName;
+    String LastName;
+    int fatherId;
+    int motherId;
+
+    /*
+     * get data from database
+     * create person object per record
+     * create person tree based on the data
+     * output tree
+     * set a point of reference
+     * search tree based on reference (parents, kids, grandkids, cousins, etc)
+     */
     public static void main (String[] args) {
         //HashMap<Person> person = new HashMap<Person>();
 
-        Person person;
-        person = new Person();
+        Person person = new Person();
+
+        DBI connect = new DBI();
+        connect.getData();
         
     }
+
+    //private static Person 
 }
--- person/Person.java
+++ person/Person.java
@@ -1,8 +1,17 @@
 package person;
 
 public class Person {
-    public String name = "Lorenzo";
+    Person father;
+    Person mother;
+    Person[] child;
+
+    public int id;
+    public String FirstName;
+    public String LastName;
+    public int fatherId;
+    public int motherId;
+
     public Person() {
-        System.out.println("Constructor of " + this.name);
+        System.out.println("Constructor of Person");
     }
 }
